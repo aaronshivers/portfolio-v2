1682dcfef2d5947979e5aff7f04673d086038fdc
aaron shivers <aaorn@moonmail.org>
2019-07-15 20:16:35 -0500

    got Other component working

--- src/assets/other.js
+++ src/assets/other.js
@@ -1,10 +1,13 @@
 module.exports = [{
   name: 'GitHub',
-  link: 'https://github.com/aaronshivers'
+  link: 'https://github.com/aaronshivers',
+  icon: '<i class="fab fa-github"></i>'
 }, {
   name: 'LinkedIn',
-  link: 'https://www.linkedin.com/in/aaron-shivers/'
+  link: 'https://www.linkedin.com/in/aaron-shivers/',
+  icon: '<i class="fab fa-linkedin"></i>'
 }, {
   name: 'Resume',
-  link: 'https://docs.google.com/document/d/1sPzUb4KdrwGRgepia-hnnaJ3xkJInzlUs6LZ9Fnimoo/edit?usp=sharing'
+  link: 'https://docs.google.com/document/d/1sPzUb4KdrwGRgepia-hnnaJ3xkJInzlUs6LZ9Fnimoo/edit?usp=sharing',
+  icon: '<i class="far fa-file"></i>'
 }]
