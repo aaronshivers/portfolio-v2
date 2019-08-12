e1d72109bfca2711cff431f72a57a08f2415cc49
aaronshivers <aaron@moonmail.org>
2019-08-12 18:40:30 -0500

    refactored Project

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,43 +1,37 @@
 import React from 'react'
 import { Accordion, Card, Button, ListGroup, Row, Col } from 'react-bootstrap'
 import { FaGithub } from 'react-icons/fa'
-import projects from '../assets/projects'
 
-const Project = () => (
-  <Accordion className="mb-3">
-    {
-      projects.map(({ name, link, code, description }, i) => (
-        <Card key={ i }>
-          <Card.Header className="text-center">
-            <Accordion.Toggle
-              as={ Button }
-              variant="link"
-              eventKey={ i }
-            >
-              { name }
-            </Accordion.Toggle>
-          </Card.Header>
+const Project = ({ index, name, link, code, description }) => (
 
-          <Accordion.Collapse eventKey={ i }>
-            <Card.Body>
-              <Row>
-                <Col className="text-center">
-                  <a href={ link }>View Project</a>
-                </Col>
-                <Col className="text-center">
-                  <a href={ code }>
-                    <FaGithub />
-                    <span> View Code</span>
-                  </a>
-                </Col>
-              </Row>
-              <p className="mt-3">{ description }</p>
-            </Card.Body>
-          </Accordion.Collapse>
-        </Card>
-      ))
-    }
-  </Accordion>
+  <Card>
+    <Card.Header className="text-center">
+      <Accordion.Toggle
+        as={ Button }
+        variant="link"
+        eventKey={ index }
+      >
+        { name }
+      </Accordion.Toggle>
+    </Card.Header>
+
+    <Accordion.Collapse eventKey={ index }>
+      <Card.Body>
+        <Row>
+          <Col className="text-center">
+            <a href={ link }>View Project</a>
+          </Col>
+          <Col className="text-center">
+            <a href={ code }>
+              <FaGithub />
+              <span> View Code</span>
+            </a>
+          </Col>
+        </Row>
+        <p className="mt-3">{ description }</p>
+      </Card.Body>
+    </Accordion.Collapse>
+  </Card>
 )
 
 export default Project
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import { Accordion } from 'react-bootstrap'
 import Project from './Project'
+import projects from '../assets/projects'
 
 const Projects = () => (
   <div>
     <h3 className="text-center">Projects</h3>
-    <Project />
+    <Accordion className="mb-3">
+      {
+        projects.map((project, i) => (
+          <Project
+            key={ i }
+            index={ i }
+            { ...project }
+          />
+        ))
+      }
+    </Accordion>
   </div>
 )
 
