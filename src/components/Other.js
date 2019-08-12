dcd36b0f61f5c4ae42fd1c358197e089814dbfeb
aaronshivers <aaron@moonmail.org>
2019-08-12 18:30:17 -0500

    refactored Other

--- src/components/Other.js
+++ src/components/Other.js
@@ -1,11 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Card, Accordion, Button, ListGroup } from 'react-bootstrap'
+import { FaGithub, FaLinkedin, FaFile } from 'react-icons/fa'
 import OtherItem from './OtherItem'
 
-const Other = () => (
-  <div>
-    <h2 className="text-center">Other</h2>
-    <OtherItem />
-  </div>
-)
+const otherData = [{
+  name: 'GitHub',
+  link: 'https://github.com/aaronshivers',
+  icon: <FaGithub />
+}, {
+  name: 'LinkedIn',
+  link: 'https://www.linkedin.com/in/aaron-shivers/',
+  icon: <FaLinkedin />
+}, {
+  name: 'Resume',
+  link: 'https://docs.google.com/document/d/1sPzUb4KdrwGRgepia-hnnaJ3xkJInzlUs6LZ9Fnimoo/edit?usp=sharing',
+  icon: <FaFile />
+}]
+
+const Other = () => {
+  const [ other ] = useState(otherData)
+
+  return (
+    <div>
+      <h2 className="text-center">Other</h2>
+      <Accordion
+        defaultActiveKey="0"
+        className="mb-3 border-bottom"
+      >
+        <Card>
+          <Card.Header className="text-center">
+            <Accordion.Toggle
+              as={ Button }
+              variant="link"
+              eventKey="0"
+            >
+              View Other Items
+            </Accordion.Toggle>
+          </Card.Header>
+
+          <Accordion.Collapse eventKey="0">
+            <Card.Body>
+              <ListGroup variant="flush">
+                {
+                  other.map((otherItem, i) => (
+                    <OtherItem key={ i } { ...otherItem } />
+                  ))
+                }
+              </ListGroup>
+            </Card.Body>
+          </Accordion.Collapse>
+        </Card>
+      </Accordion>
+    </div>
+  )
+}
 
 export default Other
--- src/components/OtherItem.js
+++ src/components/OtherItem.js
@@ -1,66 +1,17 @@
-import React, { useState } from 'react'
-import { Card, Accordion, Button, ListGroup } from 'react-bootstrap'
-import { FaGithub, FaLinkedin, FaFile } from 'react-icons/fa'
+import React from 'react'
+import { ListGroup } from 'react-bootstrap'
 
-const otherData = [{
-  name: 'GitHub',
-  link: 'https://github.com/aaronshivers',
-  icon: <FaGithub />
-}, {
-  name: 'LinkedIn',
-  link: 'https://www.linkedin.com/in/aaron-shivers/',
-  icon: <FaLinkedin />
-}, {
-  name: 'Resume',
-  link: 'https://docs.google.com/document/d/1sPzUb4KdrwGRgepia-hnnaJ3xkJInzlUs6LZ9Fnimoo/edit?usp=sharing',
-  icon: <FaFile />
-}]
+const OtherItem = ({ link, icon, name }) => (
 
-const OtherItem = () => {
-  const [ other ] = useState(otherData)
-
-  return (
-
-    <Accordion
-      defaultActiveKey="0"
-      className="mb-3 border-bottom"
+  <ListGroup.Item>
+    <a
+      href={ link }
+      target="_blank"
+      rel="noreferrer"
     >
-      <Card>
-        <Card.Header className="text-center">
-          <Accordion.Toggle
-            as={ Button }
-            variant="link"
-            eventKey="0"
-          >
-            View Other Items
-          </Accordion.Toggle>
-        </Card.Header>
-
-        <Accordion.Collapse eventKey="0">
-          <Card.Body>
-            <ListGroup variant="flush">
-              {
-                other.map(({ link, icon, name }, i) => {
-
-                  return (
-                    <ListGroup.Item key={ i }>
-                      <a
-                        href={ link }
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        <span>{ icon }</span> <span>{ name }</span>
-                      </a>
-                    </ListGroup.Item>
-                  )}
-                )
-              }
-            </ListGroup>
-          </Card.Body>
-        </Accordion.Collapse>
-      </Card>
-    </Accordion>
-  )
-}
+      <span>{ icon }</span> <span>{ name }</span>
+    </a>
+  </ListGroup.Item>
+)
 
 export default OtherItem
