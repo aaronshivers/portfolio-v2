89260f7aa8f4bea92d93929a10dcdc26dfc4cf2a
aaron shivers <aaorn@moonmail.org>
2019-08-12 15:47:24 -0500

    removed unneeded packages

--- src/app.js
+++ src/app.js
@@ -1,9 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
 import App from './components/App'
-import 'bootstrap'
-import 'normalize.css/normalize.css'
 import './styles/styles.scss'
-import { fab } from '@fortawesome/fontawesome-free'
 
 render(<App />, document.getElementById('app'))
